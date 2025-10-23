ec037ad10f8d738c81fa4fbca66728bc6fab0685
grish-ka <grishka.krot@gmail.com>
2025-10-23 20:52:16 +0100

    still bugged

--- src/drivers/screen.c
+++ src/drivers/screen.c
@@ -1,5 +1,6 @@
 #include "screen.h"
 #include "ports.h"
+#include "../kernel/util.h"
 
 /* Declaration of private functions */
 int get_cursor_offset();
@@ -30,8 +31,8 @@ void kprint_at(char *message, int col, int row) {
 
     /* Loop through message and print it */
     int i = 0;
-    while (&message[i] != 0) {
-        offset = print_char(&message[i++], col, row, WHITE_ON_BLACK);
+    while (message[i] != 0) {
+        offset = print_char(message[i++], col, row, WHITE_ON_BLACK);
         /* Compute row/col for next iteration */
         row = get_offset_row(offset);
         col = get_offset_col(offset);
@@ -39,7 +40,7 @@ void kprint_at(char *message, int col, int row) {
 }
 
 void kprint(char *message) {
-    kprint_at(&message, -1, -1);
+    kprint_at(message, -1, -1);
 }
 
 
@@ -79,6 +80,22 @@ int print_char(char c, int col, int row, char attr) {
         vidmem[offset+1] = attr;
         offset += 2;
     }
+
+    /* Check if the offset is over screen size and scroll */
+    if (offset >= MAX_ROWS * MAX_COLS * 2) {
+        int i;
+        for (i = 1; i < MAX_ROWS; i++) 
+            memory_copy(get_offset(0, i) + VIDEO_ADDRESS,
+                        get_offset(0, i-1) + VIDEO_ADDRESS,
+                        MAX_COLS * 2);
+
+        /* Blank last line */
+        char *last_line = get_offset(0, MAX_ROWS-1) + VIDEO_ADDRESS;
+        for (i = 0; i < MAX_COLS * 2; i++) last_line[i] = 0;
+
+        offset -= 2 * MAX_COLS;
+    }
+
     set_cursor_offset(offset);
     return offset;
 }
