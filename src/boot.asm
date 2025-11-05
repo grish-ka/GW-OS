732384ee7bb90eb01d08ba5264ac47c0b5c6882e
grish-ka <grishka.krot@gmail.com>
2025-11-05 20:42:25 +0000

    add entire ascii

--- src/boot.asm
+++ src/boot.asm
@@ -1,3 +1,4 @@
+[org 0x7c00]
 mov ah, 0x0e
 
 mov al, 0xb0
@@ -46,7 +47,57 @@ int 0x10
 mov al, 0xb0
 int 0x10
 
+mov al, 10
+int 0x10
+mov al, 13
+
+int 0x10
+
+
+mov al, 0
+int 0x10
+ascii_loop:
+    inc al
+    int 0x10
+    cmp al, 255
+    jne ascii_loop
+
+
+
+mov bx, 0
+
+loop2:
+    mov al, 6
+    int 0x10
+    inc bx
+    cmp bx, 200
+    jne loop2
+
+
+mov al, 10
+int 0x10
+mov al, 13
+
+int 0x10
+
+; mov al, [myVar]
+; int 0x10
+mov bx, myVar
+
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp printString
+
+
+end:
 jmp $
+
+myVar:
+    db "Look ma I'm a variable printed out", 0
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
